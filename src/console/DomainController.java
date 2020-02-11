582399376e67b359eb9086003994d9708f1d24c4
Kenneth Van der haegen <Kenneth.Vanderhaegen@Student.hogent.be>
2020-02-11 11:23:30 +0100

    add java project

---
+++ src/console/DomainController.java
@@ -0,0 +1,36 @@
+package console;
+
+import java.util.Scanner;
+
+public class DomainController {
+	public static void main(String[] args) {
+		boolean isRunning = true;
+		
+		while (isRunning) {
+			System.out.println("Welkom bij Coloretto");
+			System.out.println("1) Begin spel");
+			System.out.println("2) Toon high scores");
+			System.out.println("3) Exit");
+			
+			Scanner input = new Scanner(System.in);
+			int option = input.nextInt();
+			
+			switch (option) {
+			case 1:
+				// begin spel
+				System.out.println("Begin spel");
+				break;
+			case 2:
+				// toon high scores
+				System.out.println("High scores");
+				break;
+			case 3:
+				isRunning = false;
+				break;
+			default:
+				System.out.println("Geef 1, 2 of 3 in ");
+				break;
+			}
+		}
+	}
+}
---
+++ src/domein/Player.java
@@ -0,0 +1,9 @@
+package domein;
+
+public class Player {
+	private String name;
+	
+	public Player(String name) {
+		this.name = name;
+	}
+}
