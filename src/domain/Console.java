90b8d986e38bc25df4f17adcccd876917720fb72
Joren Heyman <joren@heyman.io>
2020-02-20 20:56:16 +0100

    reformat code

--- src/domain/Console.java
+++ src/domain/Console.java
@@ -3,24 +3,25 @@ package domain;
 import java.util.Scanner;
 
 public class Console {
-    private static Scanner input = new Scanner(System.in);
+	private static Scanner input = new Scanner(System.in);
 
-    private Console() {}
+	private Console() {
+	}
 
-    public static void close() {
-        input.close();
-    }
+	public static void close() {
+		input.close();
+	}
 
-    public static String getString() {
-        try {
-            return input.nextLine();
-        } catch (Exception e) {
-            return "";
-        }
-    }
+	public static String getString() {
+		try {
+			return input.nextLine();
+		} catch (Exception e) {
+			return "";
+		}
+	}
 
-    public static int getInt() throws NumberFormatException {
-        String line = input.nextLine();
-        return Integer.parseInt(line);
-    }
+	public static int getInt() throws NumberFormatException {
+		String line = input.nextLine();
+		return Integer.parseInt(line);
+	}
 }
