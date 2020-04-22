4d7cbd46a9094f229d6b9266c3c16970644a92a5
Joren Heyman <joren@heyman.io>
2020-04-22 11:27:42 +0200

    add test javafx app

--- src/main/Startup.java
+++ src/main/Startup.java
@@ -1,9 +1,12 @@
 package main;
 
+import javafx.application.Application;
 import ui.ConsoleApplication;
+import ui.GUIApplication;
 
 class Startup {
 	public static void main(String[] args) {
-		ConsoleApplication.start();
+		//ConsoleApplication.start();
+		Application.launch(GUIApplication.class, args);
 	}
 }
